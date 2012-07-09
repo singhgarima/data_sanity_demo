24ab291cf19079668adcb5897a0c985214c4df51
Garima Singh <garima.slideshare@gmail.com>
2012-07-09 11:46:06 +0530

    Garima - making uploadable car model

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.6'
 gem 'sqlite3'
-gem 'data_sanity', '0.1.7'
+gem 'data_sanity', '0.2.4'
 gem 'rake', '0.8.7'
+gem 'uploadable', :path => '/Users/garimas/Work/Personal/uploadable'
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ActiveRecord::Base
+  uploadable :mandatory_fields => [:company, :model], :optional_fields => [:price, :category]
   validates_presence_of :company, :model
   validates_numericality_of :price, :greater_than => 100000
   validates_inclusion_of :category, :in => %w(two three four)
